c8b0af892c87463349ee67f66feb8f3d6c0d905d
Paladetch Usavasereelert <paladetch.u@gmail.com>
2017-10-09 13:55:15 +0900

    Init repo

---
+++ modernreact/ReduxSimpleStarter/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const API_KEY = 'AIzaSyDQyBSfpNmP4APALp5aSxKlLwTQF9TJtgE'
+
+const App = () => {
+    return <div>Hi!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
